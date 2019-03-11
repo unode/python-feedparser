ed060223ed594157550fd0c25e7421833134f9a1
Harry O'connor <31008224+harrysprogramming@users.noreply.github.com>
2019-03-11 21:58:49 +0000

    Add Functionality for newer python versions

--- setup.py
+++ setup.py
@@ -33,6 +33,7 @@ setup(
         'Programming Language :: Python :: 3.0',
         'Programming Language :: Python :: 3.1',
         'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.5',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: Text Processing :: Markup :: XML',
     ],
